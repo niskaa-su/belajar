3abaecf2834909f60d551d3851af74971564e8fe
niska <115683233+niskaa-su@users.noreply.github.com>
2023-10-13 10:12:19 +0700

    Add files via upload

---
+++ Operator Aritmatika/aritmatika/src/aritmatika/Aritmatika.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aritmatika;
+
+/**
+ *
+ * @author user
+ */
+public class Aritmatika {
+    public static void main(String[] args) {
+        // buat variabel
+        int a= 12;
+        int b= 8;
+        int c= 5;
+        // hitung
+        int hasil1= a+b-c;
+        int hasil2= a*b/c;
+        int hasil3= a+b*c;
+        int hasil4= a+b/c;
+        int hasil5= (a+b)*c;
+        int hasil6= (a-b)*c;
+        // print
+        System.out.println(hasil1);
+        System.out.println(hasil2);
+        System.out.println(hasil3);
+        System.out.println(hasil4);
+        System.out.println(hasil5);
+        System.out.println(hasil6);
+    }
+    
+}
