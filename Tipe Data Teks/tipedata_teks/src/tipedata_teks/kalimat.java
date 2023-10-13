2948ed6876a8f376e5ce2c671e091bf7f094b4e8
niska <115683233+niskaa-su@users.noreply.github.com>
2023-10-13 10:16:08 +0700

    Add files via upload

---
+++ Tipe Data Teks/tipedata_teks/src/tipedata_teks/kalimat.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata_teks;
+
+/**
+ *
+ * @author user
+ */
+public class kalimat {
+    public static void main(String [] args){
+        String subjek= "Saya";
+        String prediket=" menulis";
+        String objek=" program java";
+        String keterangan=" hari ini";
+        String kalimat= subjek+prediket+objek+keterangan;
+        // print kalimat
+        System.out.println(kalimat);
+        System.out.println(subjek+prediket+objek+keterangan);
+    }
+}
---
+++ Tipe Data Teks/tipedata_teks/src/tipedata_teks/salam.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tipedata_teks;
+
+/**
+ *
+ * @author user
+ */
+public class salam {
+    public static void main(String[] args) {
+        // buat karakter
+        char c1='S';
+        char c2='A';
+        char c3='L';
+        char c4='A';
+        char c5='M';
+        //print
+        System.out.println(""+c1+c2+c3+c4+c5);
+        System.out.println(""+c5+c4+c3+c2+c1);
+    }
+    
+}
