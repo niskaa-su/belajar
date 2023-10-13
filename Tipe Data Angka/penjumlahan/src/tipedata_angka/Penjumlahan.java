b6f1c03b0d49591245ac7f2ab6feb9c87c806ab4
niska <115683233+niskaa-su@users.noreply.github.com>
2023-10-13 10:14:48 +0700

    Add files via upload

---
+++ Tipe Data Angka/penjumlahan/src/tipedata_angka/Penjumlahan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tipedata_angka;
+
+/**
+ *
+ * @author user
+ */
+public class Penjumlahan {
+    public static void main(String[] args) {
+        int nilaiA= 1500;
+        int nilaiB= 75;
+        int jumlah= nilaiA + nilaiB;
+        // lenkapilah pada baris ini
+        System.out.println("Nilai A = " + nilaiA);
+        System.out.println("Nilai B = " + nilaiB);
+        System.out.println("Jumlah = " + jumlah);
+    }
+    
+}
---
+++ Tipe Data Angka/penjumlahan/src/tipedata_angka/pembagian.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata_angka;
+
+/**
+ *
+ * @author user
+ */
+public class pembagian {
+    public static void main (String [] args){
+        float a= 20, b= 8;
+        float hasilBagi = a/b;
+        // lengkapilah kode pada baris di atas
+        System.out.println("A = " + a);
+        System.out.println("B = " + b);
+        System.out.println("A dibagi B= " + hasilBagi);
+    }
+}
