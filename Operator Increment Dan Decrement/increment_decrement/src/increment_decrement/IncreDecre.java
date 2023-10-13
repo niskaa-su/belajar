61f6d2e7b12f3cbea0585c551fb074d56dbf8453
niska <115683233+niskaa-su@users.noreply.github.com>
2023-10-13 10:12:56 +0700

    Add files via upload

---
+++ Operator Increment Dan Decrement/increment_decrement/src/increment_decrement/IncreDecre.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package increment_decrement;
+
+/**
+ *
+ * @author user
+ */
+public class IncreDecre {
+    public static void main(String[] args) {
+        // buat variabel awal
+        int a=0;
+        int b=0;
+        int c=9;
+        int d=9;
+        // print ke 1
+        System.out.println("Print ke 1");
+        System.out.println("A = " + a++);
+        System.out.println("B = " + ++b);
+        System.out.println("C = " + c--);
+        System.out.println("D = " + --d);
+        // print ke 2
+        System.out.println("Print ke 2");
+        System.out.println("A = " + a++);
+        System.out.println("B = " + ++b);
+        System.out.println("C = " + c--);
+        System.out.println("D = " + --d);
+        // print ke 3
+        System.out.println("Print ke 3");
+        System.out.println("A = " + a++);
+        System.out.println("B = " + ++b);
+        System.out.println("C = " + c--);
+        System.out.println("D = " + --d);
+        // print ke 4
+        System.out.println("Print ke 4");
+        System.out.println("A = " + a++);
+        System.out.println("B = " + ++b);
+        System.out.println("C = " + c--);
+        System.out.println("D = " + --d);
+    }
+    
+}
