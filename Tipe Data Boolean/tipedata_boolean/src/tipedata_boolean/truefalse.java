e2e67af7bbe76e6a583d9a883546aebef403dcef
niska <115683233+niskaa-su@users.noreply.github.com>
2023-10-13 10:15:24 +0700

    Add files via upload

---
+++ Tipe Data Boolean/tipedata_boolean/src/tipedata_boolean/truefalse.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tipedata_boolean;
+
+/**
+ *
+ * @author user
+ */
+public class truefalse {
+    public static void main(String[] args) {
+        String nama="Budi";
+        int nilai=54;
+        boolean lulus = false;
+        //masukkan
+        System.out.println("Nama = " + nama);
+        System.out.println("Nilai = " + nilai);
+        System.out.println("Status = " + lulus);
+    }
+    
+}
